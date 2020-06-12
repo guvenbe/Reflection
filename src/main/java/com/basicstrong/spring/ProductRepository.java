624f3cd7b42ec9b63095730fc453c77581a0c28b
Bora Guven <guvenbe@gmail.com>
2020-06-12 03:24:46 -0400

    ioc framework

--- src/main/java/com/basicstrong/spring/ProductRepository.java
+++ src/main/java/com/basicstrong/spring/ProductRepository.java
@@ -1,4 +1,15 @@
 package com.basicstrong.spring;
 
+import java.util.List;
+
 public class ProductRepository {
+    public List<Product> getPrice(List<Product> items) {
+        for (Product product : items) {
+            //let's assume reading from database
+            Double price = (double) Math.round((30 * Math.random()));
+            System.out.println("Original Price of " + product.getName() + " is " + price +" $.");
+            product.setPrice(price);
+        }
+        return items;
+    }
 }
